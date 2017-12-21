022a16cb08a011d505ed8067b8b9faea4841726e
korahtmalayil <korahtmalayil@gmail.com>
2017-12-21 12:36:21 +0530

    One_Hot_Encoded : Cleaned

--- One_Hot_Encode.py
+++ One_Hot_Encode.py
@@ -6,16 +6,16 @@ Created on 15-Dec-2017
 import pandas as pd
 from sklearn.externals import joblib 
 
-def OneHotTransform_test(X_test, datapath, one_hot_encoder_name, column_names_ohe):    
-    ohe_dict = joblib.load(datapath + one_hot_encoder_name) 
-    column_name = joblib.load(datapath + column_names_ohe)     
-    X_test_1 = pd.DataFrame()  
-    for col in ohe_dict:    
-        ohe = ohe_dict[col]
-        temp = ohe.transform(X_test[[col]])
+def OneHotTransform_test(X, datapath, one_hot_encoder_name, column_names_encoded):    
+    one_hot_dict = joblib.load(datapath + one_hot_encoder_name) 
+    column_name = joblib.load(datapath + column_names_encoded)     
+    X_df = pd.DataFrame()  
+    for col in one_hot_dict:    
+        one_hot_encoding = one_hot_dict[col]
+        temp = one_hot_encoding.transform(X_test[[col]])
         temp = pd.DataFrame(temp.toarray())
-        temp = temp.set_index(X_test.index.values)
-        X_test_1=pd.concat([X_test_1,temp],axis=1) 
-    X_test_1 = pd.concat([X_test_1, X_test.Rating], axis = 1)
-    X_test_1.columns = column_name
-    return X_test_1
+        temp = temp.set_index(X.index.values)
+        X_df = pd.concat([X_df, temp], axis=1) 
+    X_df = pd.concat([X_df, X.Rating], axis = 1)
+    X_df.columns = column_name
+    return X_df
